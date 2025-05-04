911916f87e53184c3b51874288a912b61c920a9a
Ramon <ramonempresa2001@gmail.com>
2025-05-04 20:08:30 -0300

    fix: carousel integration fix

--- js/main.js
+++ js/main.js
@@ -2,15 +2,19 @@ import { createCarousel, setupCarousel } from './components/carousel.js';
 import { aves, mamiferos, peixes } from './data/animals.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const avesCarousel = document.querySelector('#aves .carousel');
-  const mamiferosCarousel = document.querySelector('#mamiferos .carousel');
-  const peixesCarousel = document.querySelector('#peixes .carousel');
+    const avesCarousel = document.querySelector('#aves .carousel');
+    const mamiferosCarousel = document.querySelector('#mamiferos .carousel');
+    const peixesCarousel = document.querySelector('#peixes .carousel');
 
-  avesCarousel.innerHTML = createCarousel(aves);
-  mamiferosCarousel.innerHTML = createCarousel(mamiferos);
-  peixesCarousel.innerHTML = createCarousel(peixes);
+    if (avesCarousel && mamiferosCarousel && peixesCarousel) {
+        avesCarousel.innerHTML = createCarousel(aves);
+        mamiferosCarousel.innerHTML = createCarousel(mamiferos);
+        peixesCarousel.innerHTML = createCarousel(peixes);
 
-  setupCarousel(avesCarousel);
-  setupCarousel(mamiferosCarousel);
-  setupCarousel(peixesCarousel);
+        setupCarousel(avesCarousel);
+        setupCarousel(mamiferosCarousel);
+        setupCarousel(peixesCarousel);
+    } else {
+        console.error("One or more carousel divs not found.");
+    }
 });
