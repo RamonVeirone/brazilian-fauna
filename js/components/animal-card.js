ea88dcb5f64a9fc7a38f178a8bd2a63cbbc15fc8
Ramon <ramonempresa2001@gmail.com>
2025-05-04 20:33:40 -0300

    fix: images problem solved

--- js/components/animal-card.js
+++ js/components/animal-card.js
@@ -1,7 +1,7 @@
 export function createAnimalCard(name, image, description) {
   return `
       <div class="animal-card">
-          <img src="assets/images/${image}" alt="${name}">
+          <img src="js/assets/images/${image}" alt="${name}">
           <h3>${name}</h3>
           <p>${description}</p>
       </div>
