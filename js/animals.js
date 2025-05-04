2dd82eba3d2be000abda13b8bddb9dc424a8064c
Ramon <ramonempresa2001@gmail.com>
2025-05-04 19:48:37 -0300

    feat: add the animals info for birds, fishes and mammals

---
+++ js/animals.js
@@ -0,0 +1,17 @@
+export const aves = [
+    { name: "Arara-Azul", image: "arara.jpg", description: "Vive na Amazônia e tem penas coloridas!" },
+    { name: "Tucano", image: "tucano.jpg", description: "Seu bico grande é leve e forte." },
+    { name: "Beija-Flor", image: "beija-flor.jpg", description: "Bate asas rapidinho para voar!" }
+  ];
+  
+  export const mamiferos = [
+    { name: "Onça-Pintada", image: "onca.jpg", description: "O maior felino do Brasil!" },
+    { name: "Tamanduá-Bandeira", image: "tamandua.jpg", description: "Usa o focinho para comer formigas." },
+    { name: "Capivara", image: "capivara.jpg", description: "Adora nadar e vive em grupo!" }
+  ];
+  
+  export const peixes = [
+    { name: "Piranha", image: "piranha.jpg", description: "Tem dentes afiados e vive em rios." },
+    { name: "Tambaqui", image: "tambaqui.jpg", description: "Gosta de frutas que caem na água!" },
+    { name: "Peixe-Boi", image: "peixe-boi.jpg", description: "É gentil e come plantas aquáticas." }
+  ];
